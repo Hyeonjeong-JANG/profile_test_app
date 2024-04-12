2b5d0cbfdfd0bba89fb580a38a2d4917fafc7d45
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-04-12 09:08:03 +0900

    나의 첫 플러터 프로젝트

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:profile_test_app/theme.dart';
+
+import 'components/profile_buttons.dart';
+import 'components/profile_count_info.dart';
+import 'components/profile_drawer.dart';
+import 'components/profile_header.dart';
+import 'components/profile_tab.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: ProfileDrawer(),
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          ProfileHeader(),
+          SizedBox(height: 20),
+          ProfileCountInfo(),
+          SizedBox(height: 20),
+          ProfileButtons(),
+          Expanded(child: ProfileTab()),
+        ],
+      ),
+    );
+  }
+}
+
+AppBar _buildProfileAppBar() {
+  return AppBar();
+}
